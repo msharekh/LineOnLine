c67bf898c09a0f1d2690ed747edd542808df601c
Mishal Alsharekh <mishal.sharekh@gmail.com>
2021-08-28 21:51:10 +0300

    enable navigation between them with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// import { AppRoutingModule } from './app-routing.module';
+import { appRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+
 import { RestaurantComponent } from './restaurant/restaurant.component';
 import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+
     RestaurantComponent,
     ProductComponent
   ],
   imports: [
-    BrowserModule
-    // AppRoutingModule
+    BrowserModule,
+    appRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
