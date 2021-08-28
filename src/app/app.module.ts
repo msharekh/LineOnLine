9193d28f04c6bac05b655f52c0b98fb3406c97d7
Mishal Alsharekh <mishal.sharekh@gmail.com>
2021-08-28 21:27:05 +0300

    product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RestaurantComponent } from './restaurant/restaurant.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
