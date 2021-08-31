743bac05d4667de337d240419dea0866f25448c8
Mishal Alsharekh <mishal.sharekh@gmail.com>
2021-09-01 01:35:40 +0300

    consume with error

--- src/app/cuisine/cuisine.component.ts
+++ src/app/cuisine/cuisine.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-cuisine',
   templateUrl: './cuisine.component.html',
   styleUrls: ['./cuisine.component.css']
 })
 export class CuisineComponent implements OnInit {
-
-  constructor() { }
+  products = [];
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.sendGetRequest().subscribe((data: any[]) => {
+      console.log(data);
+      this.products = data;
+    })
   }
 
 }
