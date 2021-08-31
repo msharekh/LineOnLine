7c1a9fc8b8037f687b8b2f670fac440ce46e48b3
Mishal <mishal.sharekh@gmail.com>
2021-09-01 01:54:41 +0300

    working

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
-  private REST_API_SERVER = "http://localhost:3000";
+  private REST_API_SERVER = "http://localhost:3000/products";
   constructor(private httpClient: HttpClient) { }
   public sendGetRequest() {
     return this.httpClient.get(this.REST_API_SERVER);
