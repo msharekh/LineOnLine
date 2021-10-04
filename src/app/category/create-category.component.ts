2aadd8985cec60f6cf3d91fc86c4acea0539f524
Mishal Alsharekh <mishal.sharekh@gmail.com>
2021-10-05 00:02:47 +0300

    categoryForm

--- src/app/category/create-category.component.ts
+++ src/app/category/create-category.component.ts
@@ -8,14 +8,40 @@ import { FormControl, FormControlName, FormGroup } from '@angular/forms';
 })
 export class CreateCategoryComponent implements OnInit {
 
-  categoryForm : FormGroup;
+  categoryForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
     this.categoryForm = new FormGroup({
       categoryname: new FormControl(),
       restaurantname: new FormControl(),
+      details: new FormGroup({
+        a1:new FormControl(),
+        a2:new FormControl(),
+        a3:new FormControl()
+      })
     });
+  }
+
+  onSubmit(): void {
+    console.log(this.categoryForm);
+    console.log(this.categoryForm.get('categoryname'));
+    console.log(this.categoryForm.controls.categoryname.touched);
+
+  }
+
+  onLoadDataClick(): void {
+    console.log(this.categoryForm);
+
+    this.categoryForm.setValue({
+      categoryname: 'Ch Burger',
+      restaurantname: 'Hardeez',
+      details:{
+        a1:'11111',
+        a2:'22222',
+        a3:'33333'
+      }
+    })
   }
 
 }
