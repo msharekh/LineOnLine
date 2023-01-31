222bd1da178704314572b4a9cc907806fed60677
Mishal Alsharekh <mishal.sharekh@gmail.com>
2023-02-01 01:51:59 +0300

    fixed but db service not working

--- events.js
+++ events.js
@@ -1,4 +1,4 @@
-const express = require('express');
+//const express = require('express');
 
 function createRouter(db) {
   const router = express.Router();
--- server.js
+++ server.js
@@ -2,27 +2,95 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const events = require('./events');
+//const events = require('./events');
 
 const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'timeline',
-    password : 'password',
-    database : 'timeline'
+  host: 'localhost',
+  user: 'timeline',
+  password: 'password',
+  database: 'timeline'
+});
+
+connection.connect();
+
+const port = process.env.PORT || 8080;
+
+const app = express()
+  .use(cors())
+  .use(bodyParser.json())
+  .use(events(connection));
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
+
+
+function createRouter(db) {
+  const router = express.Router();
+  const owner = '';
+
+  // the routes are defined here
+
+  router.post('/event', (req, res, next) => {
+    db.query(
+      'INSERT INTO events (owner, name, description, date) VALUES (?,?,?,?)',
+      [req.body.owner, req.body.name, req.body.description, new Date(req.body.date)],
+      (error) => {
+        if (error) {
+          console.error(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json({status: 'ok event'});
+        }
+      }
+    );
   });
-  
-  connection.connect();
-  
-  const port = process.env.PORT || 8080;
-  
-  const app = express()
-    .use(cors())
-    .use(bodyParser.json())
-    .use(events(connection));
-  
-  app.listen(port, () => {
-    console.log(`Express server listening on port ${port}`);
+
+  router.get('/event', function (req, res, next) {
+    db.query(
+      'SELECT id, name, description, date FROM events WHERE owner=? ORDER BY date LIMIT 10 OFFSET ?',
+      [owner, 10*(req.params.page || 0)],
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
   });
+
+  router.put('/event/:id', function (req, res, next) {
+    db.query(
+      'UPDATE events SET name=?, description=?, date=? WHERE id=? AND owner=?',
+      [req.body.name, req.body.description, new Date(req.body.date), req.params.id, owner],
+      (error) => {
+        if (error) {
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json({status: 'ok'});
+        }
+      }
+    );
+  });
+
+  router.delete('/event/:id', function (req, res, next) {
+    db.query(
+      'DELETE FROM events WHERE id=? AND owner=?',
+      [req.params.id, owner],
+      (error) => {
+        if (error) {
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json({status: 'ok'});
+        }
+      }
+      
+    );
+  });
+  return router;
+}
 
 // var express = require('express');
 
